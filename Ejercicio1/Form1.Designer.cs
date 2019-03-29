912323fbbfe0b2cb3e186150d95b2f1f0c8cc403
Operador <Operador@VA107-026>
2019-03-29 18:45:21 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEmpleado = new System.Windows.Forms.Button();
+            this.lblNombres = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.txtAtencion = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblEmpleados = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(29, 264);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(572, 118);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CATEGORIA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MINUTOS DE TARDANZA:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(379, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "N°LLAMADAS DE ATENCION: ";
+            // 
+            // btnEmpleado
+            // 
+            this.btnEmpleado.Location = new System.Drawing.Point(29, 208);
+            this.btnEmpleado.Name = "btnEmpleado";
+            this.btnEmpleado.Size = new System.Drawing.Size(186, 23);
+            this.btnEmpleado.TabIndex = 5;
+            this.btnEmpleado.Text = "REGISTRAR EMPLEADO";
+            this.btnEmpleado.UseVisualStyleBackColor = true;
+            this.btnEmpleado.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNombres
+            // 
+            this.lblNombres.AutoSize = true;
+            this.lblNombres.Location = new System.Drawing.Point(38, 23);
+            this.lblNombres.Name = "lblNombres";
+            this.lblNombres.Size = new System.Drawing.Size(61, 13);
+            this.lblNombres.TabIndex = 6;
+            this.lblNombres.Text = "NOMBRES";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(199, 20);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(100, 20);
+            this.txtNombres.TabIndex = 7;
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Location = new System.Drawing.Point(183, 159);
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(100, 20);
+            this.txtMinutos.TabIndex = 8;
+            // 
+            // txtAtencion
+            // 
+            this.txtAtencion.Location = new System.Drawing.Point(554, 159);
+            this.txtAtencion.Name = "txtAtencion";
+            this.txtAtencion.Size = new System.Drawing.Size(100, 20);
+            this.txtAtencion.TabIndex = 9;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(481, 207);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lblEmpleados
+            // 
+            this.lblEmpleados.AutoSize = true;
+            this.lblEmpleados.Location = new System.Drawing.Point(437, 400);
+            this.lblEmpleados.Name = "lblEmpleados";
+            this.lblEmpleados.Size = new System.Drawing.Size(100, 13);
+            this.lblEmpleados.TabIndex = 11;
+            this.lblEmpleados.Text = "NRO EMPLEADOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 422);
+            this.Controls.Add(this.lblEmpleados);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtAtencion);
+            this.Controls.Add(this.txtMinutos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.lblNombres);
+            this.Controls.Add(this.btnEmpleado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEmpleado;
+        private System.Windows.Forms.Label lblNombres;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.TextBox txtAtencion;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblEmpleados;
+    }
+}
+
---
+++ Ejercicio1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        CEmpleado objempleado = null; 
+        public Form1()
+        {
+            InitializeComponent();
+            llenarcombocategorias();
+            configurarlistaempleados();
+        }
+        void llenarcombocategorias()
+        {
+            comboBox1.Items.Add("A");
+            comboBox1.Items.Add("B");
+            comboBox1.Items.Add("C");
+            comboBox1.Items.Add("D");
+        }
+        void configurarlistaempleados()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Columns.Add("Empleado",120);
+            listView1.Columns.Add("Categoria", 80);
+            listView1.Columns.Add("Minutos tardanza", 120);
+            listView1.Columns.Add("Llamadas atencion", 120);
+            listView1.Columns.Add("Pago", 120);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            objempleado = new CEmpleado();
+            objempleado.Nombres = txtNombres.Text;
+            objempleado.Categoria = comboBox1.Text;
+            objempleado.Tardanza = Convert.ToInt32(txtMinutos.Text);
+            objempleado.Atencion = Convert.ToInt32(txtAtencion.Text);
+            ListViewItem fila = new ListViewItem(objempleado.Nombres);
+            fila.SubItems.Add(objempleado.Categoria);
+            fila.SubItems.Add(objempleado.Tardanza.ToString());
+            fila.SubItems.Add(objempleado.Atencion.ToString());
+            fila.SubItems.Add(objempleado.Calcular_pago().ToString());
+            listView1.Items.Add(fila);
+            lblNombres.Text = listView1.Items.Count.ToString();
+            txtNombres.Clear();
+            txtMinutos.Clear();
+            txtAtencion.Clear();
+            comboBox1.SelectedIndex = -1;
+            txtNombres.Focus();
+          
+
+        }
+    }
+}
